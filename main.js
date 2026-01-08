964e5d237452560fd48a13ebbc6a71518fb2a9e3
Geetanjali Vishwakarma <150675264+thegeetanjali@users.noreply.github.com>
2026-01-08 22:12:16 +0530

    Add files via upload
    Initial upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  desktopCapturer,
+  dialog,
+} = require("electron");
+const fs = require("fs");
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 550,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.removeMenu();
+  win.loadFile("index.html");
+  //win.webContents.openDevTools();
+
+  ipcMain.on("load-page", (event, page) => {
+    win.loadFile(page);
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
